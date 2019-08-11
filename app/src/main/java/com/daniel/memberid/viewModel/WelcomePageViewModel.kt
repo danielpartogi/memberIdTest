f72ee78544d29a894e9e7873f4154f4760bcdcc2
daniel <daniel.partogi@sangkuriang.co.id>
2019-08-12 01:58:32 +0700

    error email handling

--- app/src/main/java/com/daniel/memberid/viewModel/WelcomePageViewModel.kt
+++ app/src/main/java/com/daniel/memberid/viewModel/WelcomePageViewModel.kt
@@ -1,23 +1,25 @@
 package com.daniel.memberid.viewModel
 
 import android.app.Application
+import androidx.databinding.ObservableBoolean
 import androidx.databinding.ObservableField
 import androidx.lifecycle.AndroidViewModel
 import com.daniel.memberid.SingleLiveEvent
 
 class WelcomePageViewModel(application: Application) : AndroidViewModel(application) {
 
-     val email: ObservableField<String> = ObservableField("")
-     val error: SingleLiveEvent<Boolean> = SingleLiveEvent()
-     val login : ObservableField<Boolean> = ObservableField(false)
+    val email: ObservableField<String> = ObservableField("")
+    val error: SingleLiveEvent<Boolean> = SingleLiveEvent()
+    val isError: ObservableBoolean = ObservableBoolean(false)
 
     fun login(){
         if(email.get()!="danielpartogi1996@gmail.com"){
             error.value = true
+            isError.set(true)
         }
         else {
-            login.set(true)
             error.value = false
+            isError.set(false)
         }
     }
 
